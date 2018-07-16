c19175db026cdb26d2eb7271be0a438c624d9d13
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-16 09:25:40 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,8 +28,6 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
  
-puts epic_tragedy
-  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
